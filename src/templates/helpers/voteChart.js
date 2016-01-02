3b7afb8f4abda8b30379eb9cb451ef5d1476d9ad
Yami <yamikuronue@gmail.com>
2016-01-02 17:41:32 -0500

    DEBUG: putting in hardcoded results

--- src/templates/helpers/voteChart.js
+++ src/templates/helpers/voteChart.js
@@ -6,6 +6,9 @@ module.exports = function(percent) {
 	xml += '<rect x="0" y="0" width="' + percent + '" height="12" style="fill:rgb(0,0,0)"/>';
 	xml += '</svg>';
 	
+	/*Debug*/
+	xml = '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="12"><rect width="100%" height="100%" fill="#AC1717" /><rect x="100.00%" width="0.00%" height="100%" fill="#560000" /></svg>'
+	
 	const b64 = new Buffer(xml).toString('base64');
 	const html = '<img src="data:image/svg+xml;base64,' + b64 + '">';
 	return new Handlebars.SafeString(html);
