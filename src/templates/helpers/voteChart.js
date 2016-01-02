305db4db8ad58606b30c7b470d2a62a6841608d9
Yami <yamikuronue@gmail.com>
2016-01-02 17:32:48 -0500

    base64 encode?

--- src/templates/helpers/voteChart.js
+++ src/templates/helpers/voteChart.js
@@ -2,8 +2,11 @@
 const Handlebars = require('handlebars');
 
 module.exports = function(percent) {
-	let html = '<svg width="110" height="15"> <rect x="0" y="0" width="100" height="12" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)"/>';
-	html += '<rect x="0" y="0" width="' + percent + '" height="12" style="fill:rgb(0,0,0)"/>';
-	html += '</svg>';
+	let xml = '<svg width="110" height="15"> <rect x="0" y="0" width="100" height="12" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)"/>';
+	xml += '<rect x="0" y="0" width="' + percent + '" height="12" style="fill:rgb(0,0,0)"/>';
+	xml += '</svg>';
+	
+	const b64 = new Buffer(xml).toString('base64');
+	const html = '<img src="data:image/svg+xml;base64,' + b64 + '">';
 	return new Handlebars.SafeString(html);
 };
