34910665c1c86451b73c971e31fd830d561e8f19
Yami <yamikuronue@gmail.com>
2016-01-02 17:11:38 -0500

    are width and height mandatory on svg?

--- src/templates/helpers/voteChart.js
+++ src/templates/helpers/voteChart.js
@@ -2,7 +2,7 @@
 const Handlebars = require('handlebars');
 
 module.exports = function(percent) {
-	let html = '<svg > <rect x="0" y="0" width="100" height="12" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)"/>';
+	let html = '<svg width="110" height="15"> <rect x="0" y="0" width="100" height="12" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)"/>';
 	html += '<rect x="0" y="0" width="' + percent + '" height="12" style="fill:rgb(0,0,0)"/>';
 	html += '</svg>';
 	return new Handlebars.SafeString(html);
