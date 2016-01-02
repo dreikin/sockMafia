8311c00a3be5cffb773e5b9bdabab616cacc24ae
Yami <yamikuronue@gmail.com>
2016-01-02 17:46:10 -0500

    Now it should work

--- src/templates/helpers/voteChart.js
+++ src/templates/helpers/voteChart.js
@@ -2,12 +2,9 @@
 const Handlebars = require('handlebars');
 
 module.exports = function(percent) {
-	let xml = '<svg width="110" height="15"> <rect x="0" y="0" width="100" height="12" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)"/>';
+	let xml = '<svg xmlns="http://www.w3.org/2000/svg" width="110" height="15"> <rect x="0" y="0" width="100" height="12" style="fill:none;stroke-width:1;stroke:rgb(0,0,0)"/>';
 	xml += '<rect x="0" y="0" width="' + percent + '" height="12" style="fill:rgb(0,0,0)"/>';
 	xml += '</svg>';
-	
-	/*Debug*/
-	xml = '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="12"><rect width="100%" height="100%" fill="#AC1717" /><rect x="100.00%" width="0.00%" height="100%" fill="#560000" /></svg>'
 	
 	const b64 = new Buffer(xml).toString('base64');
 	const html = '<img src="data:image/svg+xml;base64,' + b64 + '">';
