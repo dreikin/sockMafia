fba4f05b372ced1f2ffec4a78f8b99d451c25f8b
MR Wirth <dreikin@gmail.com>
2016-01-02 23:48:19 -0500

    A few more voteChart formatting changes to match the examples.

--- src/templates/helpers/voteChart.js
+++ src/templates/helpers/voteChart.js
@@ -14,8 +14,8 @@ module.exports = function(percent) {
 		fillColor = '#005600';
 		bgColor = '#FFFFFF';
 	}
-	let xml = '<svg xmlns="http://www.w3.org/2000/svg" width="110" height="15"> <rect x="0" y="0" width="100%" height="100%" fill="' + bgColor + '"/>';
-	xml += '<rect x="' + (100.0 - percent) + '%" y="0" width="' + percent + '" height="100%" fill="' + fillColor + '"/>';
+	let xml = '<svg xmlns="http://www.w3.org/2000/svg" width="100" height="12"> <rect width="100%" height="100%" fill="' + bgColor + '"/>';
+	xml += '<rect x="' + (100.0 - percent) + '%" width="' + percent + '" height="100%" fill="' + fillColor + '"/>';
 	xml += '</svg>';
 	
 	const b64 = new Buffer(xml).toString('base64');
