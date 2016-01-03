0ffbdcabd0fa8f3b96a791def79b1c212177fc71
Yami <yamikuronue@gmail.com>
2016-01-03 11:51:44 -0500

    Adding new helper to handle the commas

---
+++ src/templates/helpers/listNames.js
@@ -0,0 +1,16 @@
+'use strict';
+const Handlebars = require('handlebars');
+
+module.exports = function(list) {
+	list = list.map((value) => {
+		if (typeof value === 'object') {
+			if (value.retracted) {
+				value = '<s>' + value.voter + '</s>';
+			} else {
+				value = value.voter;
+			}
+		}
+		return value;
+	});
+	return new Handlebars.SafeString(list.join(','));
+};
