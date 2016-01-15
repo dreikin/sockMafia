ef3d758556a501114667ff766613666b65a6d739
MR Wirth <dreikin@gmail.com>
2016-01-15 09:58:08 -0500

    Bugfix: Correct template output for rescinded votes.

--- src/templates/helpers/listNames.js
+++ src/templates/helpers/listNames.js
@@ -22,8 +22,8 @@ module.exports = function(list) {
 	list = list.map((value) => {
 		if (typeof value === 'object') {
 			if (value.retracted) {
-				value = '<a href="/t/' + slugs[Math.floor(Math.random() * slugs.length)] + '/' + value.game + '/' + value.post + '"><s>' + value.voter + ' </s></a>';
-				value += '<a href="/t/' + slugs[Math.floor(Math.random() * slugs.length)] + '/' + value.game + '/' + value.retractedAt + '">[X]</a>';
+				value = '<a href="/t/' + slugs[Math.floor(Math.random() * slugs.length)] + '/' + value.game + '/' + value.post + '"><s>' + value.voter + ' </s></a>'
+					+ '<a href="/t/' + slugs[Math.floor(Math.random() * slugs.length)] + '/' + value.game + '/' + value.retractedAt + '">[X]</a>';
 			} else {
 				value = '<a href="/t/' + slugs[Math.floor(Math.random() * slugs.length)] + '/' + value.game + '/' + value.post + '"><b>' + value.voter + '</b></a>';
 			}
